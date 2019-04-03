516af3b5df6221808a0be3bd55e29113a5f71850
= <=>
2019-04-02 23:12:28 -0400

    Can detect and remove bad MIDIs

--- pretty_midi_crasher.py
+++ pretty_midi_crasher.py
@@ -1,7 +1,21 @@
-import os
+import os, shutil
 import pretty_midi
 
-songs_directory = "data/classical/"
+# Name directory to crawl. Cannot contain subdirectories.
+songs_directory = "data/test_cases/"
+# Name directory to place bad files in.
+bad_files_directory = "removed/" + songs_directory
+# Ensure dump directory exists
+bad_files_directory_path_components = bad_files_directory.split("/")
+validated_path = ""
+for dir in bad_files_directory_path_components:
+	try:
+		os.listdir(validated_path + dir)
+	except OSError as e:
+		os.mkdir(validated_path + dir)
+	finally:
+		validated_path += dir + "/"
+
 sample_songs = [
 	"2_ase.mid",
 	"Carulli_Concerto_Flauto_Chitarra_Orchestra_Allegro.mid"
@@ -10,11 +24,17 @@ sample_songs = [
 # midi_data = pretty_midi.PrettyMIDI("data/classical/2_ase.mid")
 # midi_data = pretty_midi.PrettyMIDI("data/classical/Carulli_Concerto_Flauto_Chitarra_Orchestra_Allegro.mid")
 
-for song in sample_songs:
+# Open file for log of files removed.
+bad_files_log = open("removed_files.csv", "w")
+
+for song in os.listdir(songs_directory):
 	try:
 		midi_data = pretty_midi.PrettyMIDI(songs_directory + song)
-	except Exception as e:
+	except IOError as e:
 		print "Failed to import " + song
+		bad_files_log.write(song + "\n")
+		shutil.move(songs_directory + song, bad_files_directory + song)
 	else:
 		print "Succeeded in importing " + song
 	
+bad_files_log.close()
